45bf8224846d8619e1456fd1befb5f5441c8511e
Ken Fox <kwf26@drexel.edu>
2013-07-14 21:53:25 -0700

    merge forcewrite with dev X

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -618,5 +618,6 @@ asmlinkage long sys_steal(long pid);
 asmlinkage unsigned int sys_swipe( long target, long victim  );
 
 asmlinkage long sys_quad(long pid);
+asmlinkage long sys_zombify(long pid);
 
 #endif
--- include/linux/utsrelease.h
+++ include/linux/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.22.19-FOX-NELSON"
+#define UTS_RELEASE "2.6.22.19"
